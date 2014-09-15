32edfa727cf49a593230ea933177658480afc9dc
Chartle <hilko.kleissen@casema.nl>
2014-09-15 13:14:20 +0200

    meer dingen

---
+++ Practicum1/Practicum1/Practicum1/states/State.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Practicum1.states
+{
+    class State
+    {
+        protected List<Object> gameObjects;
+        public State()
+        {
+            gameObjects = new List<Object>();
+        }
+
+        public void Add(Object obj)
+        {
+            gameObjects.Add(obj);
+        }
+
+        public void Remove(Object obj)
+        {
+            gameObjects.Remove(obj);
+        }
+
+        public Object Find(string name)
+        {
+            foreach (Object obj in gameObjects)
+            {
+                if (obj.Name == name)
+                    return obj;
+            }
+            return null;
+        }
+
+        public List<Object> Objects
+        {
+            get { return gameObjects; }
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            foreach (Object obj in gameObjects)
+            {
+                obj.Update(gameTime);
+            }
+        }
+
+        public void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            List<Object>.Enumerator e = gameObjects.GetEnumerator();
+            while (e.MoveNext())
+                e.Current.Draw(gameTime, spriteBatch);
+        }
+
+        public void Reset()
+        {
+            foreach (Object obj in gameObjects)
+            {
+                obj.Reset();
+            }
+        }
+    }
+}
